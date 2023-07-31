8a213c414b77aa5b173c83a779022fdabdcd4b24
Daniel <contact@daniel-rubin.de>
2023-07-31 21:28:49 +0200

    feat: worked on patch and delete

--- tasks/views.py
+++ tasks/views.py
@@ -27,7 +27,7 @@ class TaskRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
         serializer = TaskSerializer(task, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response({"status": 200, "data": serializer.data}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, *args, **kwargs):
