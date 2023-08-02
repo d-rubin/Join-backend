acfecf79be1075c56847376da456162bfd99f5b3
Daniel <contact@daniel-rubin.de>
2023-08-02 19:43:41 +0200

    refactor: now using generic View from rest-framework

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,5 @@ from .views import TaskListCreateView, TaskRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('', TaskListCreateView.as_view(), name='task-list-create'),
-    path('<int:pk>/', TaskRetrieveUpdateDestroyView.as_view(), name='task-retrieve-update-destroy'),
+    path('<int:task_id>/', TaskRetrieveUpdateDestroyView.as_view(), name='task-retrieve-update-destroy'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,49 +1,23 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import generics, status
+from rest_framework import status
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 from .models import Task
 from .serializers import TaskSerializer
 
 
-class TaskListCreateView(generics.ListCreateAPIView):
+class TaskListCreateView(ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
 
-class TaskUpdateDestroySerializer:
-    pass
-
-
-class TaskRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+class TaskRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
-    
-    def get_serializer_class(self):
-        # Überschreibe die Methode, um die Serializer-Klasse basierend auf der Aktion zu wählen
-        if self.request.method == 'GET':
-            return TaskSerializer
-        elif self.request.method in ['PATCH', 'DELETE']:
-            return TaskUpdateDestroySerializer  # Hier musst du eine entsprechende Serializer-Klasse erstellen
-        return TaskSerializer
-
-    def get(self, request, *args, **kwargs):
-        queryset = self.queryset.filter(assignee=request.user)
-        serializer = TaskSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def patch(self, request, *args, **kwargs):
-        task_id = kwargs['pk']
-        task = get_object_or_404(self.queryset, id=task_id)
-        serializer = TaskSerializer(task, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"status": 200, "data": serializer.data}, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, *args, **kwargs):
-        task_id = kwargs['pk']
-        task = get_object_or_404(self.queryset, id=task_id)
-        task.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    lookup_url_kwarg = "task_id"
+    lookup_field = "id"
+    serializer_class = TaskSerializer
