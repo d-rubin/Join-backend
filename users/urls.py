86e4f55bd318e122551937e6e723f1d2f46e0a2a
Daniel <contact@daniel-rubin.de>
2023-08-04 19:22:46 +0200

    chore: removed get user endpoint

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,5 @@ from .views import ContactsView, UserView
 
 urlpatterns = [
     path('', ContactsView.as_view(), name="Contacts"),
-    path('<int:pk>/', UserView.as_view(), name='User'),
+    path('<int:user_id>/', UserView.as_view(), name='User'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
-from rest_framework import generics
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import UserSerializer
@@ -9,14 +9,6 @@ from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here.
-class ContactsView(generics.ListCreateAPIView):
+class ContactsView(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-
-class UserView(APIView):
-    def get(self, request, *args, **kwargs):
-        user_id = kwargs['pk']
-        user = get_object_or_404(User, id=user_id)
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
