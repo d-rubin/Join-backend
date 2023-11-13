85afe3918783672c2f04d92195d907a942885bf0
Daniel <contact@daniel-rubin.de>
2023-11-13 22:07:20 +0100

    fix: added url

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import TaskListCreateView, TaskRetrieveUpdateDestroyView, SubtaskCreateAPIView, SubtaskListForTaskAPIView
+from .views import TaskListCreateView, TaskRetrieveUpdateDestroyView, SubtaskCreateAPIView, SubtaskListForTaskAPIView, \
+    SubTasksList
 
 urlpatterns = [
     path('', TaskListCreateView.as_view(), name='task-list-create'),
     path('<int:task_id>/', TaskRetrieveUpdateDestroyView.as_view(), name='task-retrieve-update-destroy'),
-    path("<int:task_id>/subtasks/", SubtaskListForTaskAPIView.as_view(), name='Subtasks'),
+    path("<int:task_id>/subtasks/", SubtaskListForTaskAPIView.as_view(), name='Subtasks for Task'),
     path("subtasks/create/", SubtaskCreateAPIView.as_view(), name="Create Subtask"),
+    path("subtasks/", SubTasksList.as_view(), name="All Subtasks"),
 ]
