f41180c990814a1a8ea07c1e93c33e371a448d8b
Daniel Rubin <daniel.rubin1997@gmail.com>
2024-03-15 20:00:28 +0100

    feat: restricted allowed_hosts

--- api/settings.py
+++ api/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://join.daniel-rubin.de", "https://join-d-rubin.vercel.app", "localhost", '127.0.0.1']
+ALLOWED_HOSTS = ["https://join.daniel-rubin.de", "https://join-d-rubin.vercel.app", "206.189.49.23"]
 
 CORS_ALLOWED_ORIGINS = [
     "https://join.daniel-rubin.de",
