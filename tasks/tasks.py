d7be91e55c5feff96db73c55f9893eda95d98bfb
Daniel Rubin <daniel.rubin1997@gmail.com>
2024-03-25 19:16:06 +0100

    feat: add exeption for done tasks in send_task_reminder

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,7 +1,7 @@
 from django.template.loader import render_to_string
 from django.utils import timezone
 from api.celery import app
-from tasks.models import Task
+from tasks.models import Task, StatusChoices
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
 
@@ -9,7 +9,7 @@ from django.contrib.auth.models import User
 @app.task
 def send_task_reminder():
     today = timezone.now()
-    tasks = Task.objects.filter(due_date__lte=today)
+    tasks = Task.objects.filter(due_date__lte=today, status=not StatusChoices.DONE)
     for task in tasks:
         user = User.objects.get(username=task.assignee)
         send_mail(
